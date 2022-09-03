31b9b28240ab96ec67ec65eb1911cad801a4181e
Dominik <dominik.graf2001@gmail.com>
2022-09-03 09:05:17 +0200

    firebase bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { StartScreenComponent } from './start-screen/start-screen.component';
 
 const routes: Routes = [
   {path: '', component: StartScreenComponent},
-  {path: 'game/:id', component: GameComponent}
+  {path: 'game', component: GameComponent}
 ];
 
 @NgModule({
