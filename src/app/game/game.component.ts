512d321456e66c7294701afaffe647547ae496e2
Dominik <dominik.graf2001@gmail.com>
2022-09-03 11:20:18 +0200

    new functions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,7 +4,7 @@ import {MatDialog} from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { collectionData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import { addDoc, doc, setDoc } from 'firebase/firestore';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -17,37 +17,35 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
 currentCard: string = '';
   game: Game;
-  games$: Observable<any>;
+  gameId: string;
+
 
 constructor(private route: ActivatedRoute, private firestore: Firestore, public dialog: MatDialog) { 
    
-const coll= collection(firestore, 'games');
-this.games$ = collectionData(coll);
-
-this.games$.subscribe((newgame)=>{
-  console.log('new game',newgame )
-})
-  }
-
 
   ngOnInit(): void {
-  this.newGame();
-   this.route.params.subscribe((params)=>{
-console.log(params.id);
-this.firestore.collection().doc().subscribe((game:any)=>{
-console.log('Game update',game);
-this.game.currentPlayer=game.currentPlayer;
-})
+    this.newGame();
+    this.route.params.subscribe((params) => {
+      this.gameId = params['id'];
+      this
+        .firestore
+        .collection('games')
+        .doc(this.gameId)
+        .valueChanges()
+        .subscribe((game: any) => {
+              console.log('game update', game );
+          this.game.currentPlayer = game.currentPlayer;
+          this.game.playedCards = game.playedCards;
+          this.game.players = game.players;
+          this.game.stack = game.stack;
 
-   })
-};
+        });
+    })
+  }
+}
 
-
-async newGame(){
+newGame(){
 this.game = new Game ();
-// const coll = collection(this.firestore, 'games');
-// let gameInfo = await  addDoc(coll, this.game.toJson());
-// console.log('Das ist die Game ID',gameInfo)
 }
 
   takeCard(){
